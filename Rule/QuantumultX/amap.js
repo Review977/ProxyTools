e8de4414169c5cb4f42dc9d0c8f71d0971877856
elevencd <77088454+elevencd@users.noreply.github.com>
2023-01-04 20:19:57 +0800

    Update amap.js

--- Rule/QuantumultX/amap.js
+++ Rule/QuantumultX/amap.js
@@ -53,7 +53,7 @@ switch (adAppName(url)) {
           return (
             // item.dataKey === "AnnualBillCardV2" || // 年度报告
             item.dataKey === "MyOrderCard" || // 我的订单
-            // item.dataKey === "GdRecommendCard" || // 高德推荐
+            item.dataKey === "GdRecommendCard" || // 高德推荐
             item.dataKey === "SceneVehicleCard_recommend" || // 我的车辆
             item.dataKey === "SceneVehicleCard_function" // 我的车辆
             // item.dataKey === "PopularActivitiesCard" // 热门活动
