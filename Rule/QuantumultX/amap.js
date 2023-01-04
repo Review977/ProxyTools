65fff0038108cd2ef2144fea0e03ee4836f635ce
elevencd <77088454+elevencd@users.noreply.github.com>
2023-01-04 19:53:46 +0800

    Update amap.js

--- Rule/QuantumultX/amap.js
+++ Rule/QuantumultX/amap.js
@@ -1,16 +1,75 @@
-[filter_local]
-ip-cidr, 106.11.43.102/16, reject
-ip-cidr, 203.209.245.78/16, reject
-ip-cidr, 203.119.169.58/16, reject
+// 2023-01-04 17:42
 
-[rewrite_local]
-^https?:\/\/sns\.amap\.com\/ws\/msgbox\/pull.+ url reject-dict
-^https?:\/\/sns\.amap\.com\/ws\/oss\/maplayer\/list\/? url reject-dict
-^https?:\/\/m5.amap.com\/ws\/valueadded\/ url reject
-^https?+:\/\/m\d\.amap\.com\/ws\/valueadded\/alimama\/splash_screen\/ url reject-200
-^https?:\/\/m\d\.amap\.com\/ws\/valueadded\/splash_screen url reject-200
-^http?:\/\/optimus-ads\.amap\.com\/uploadimg\/ url reject
+var body = $response.body;
+var method = $request.method;
+var url = $request.url;
 
+if (!body) $done({});
 
-[mitm] 
-hostname=optimus-ads.amap.com, m5.amap.com, sns.amap.com, md.amap.com
+function adAppName(adUrls) {
+  
+  if (/^https:\/\/m5\.amap\.com\/ws\/faas\/amap-navigation\/main-page(-location)?\?/.test(adUrls)) return "高德地图-首页卡片";
+  if (/^https:\/\/m5\.amap\.com\/ws\/faas\/amap-navigation\/main-page-assets\?/.test(adUrls)) return "高德地图-首页消息";
+  if (/^https:\/\/sns\.amap\.com\/ws\/msgbox\/pull(3|_mp)\?/.test(adUrls)) return "高德地图-首页消息";
+  if (/^https:\/\/m5\.amap\.com\/ws\/shield\/dsp\/profile\/index\/nodefaasv3\?/.test(adUrls)) return "高德地图-我的";
+  return "";
+}
+
+switch (adAppName(url)) {
+  case "高德地图-首页卡片":
+    try {
+      let obj = JSON.parse(body);
+      if (obj.data && obj.data.cardList) {
+        obj.data.cardList = obj.data.cardList.filter((item) => {
+          return item.dataKey === "LoginCard";
+        });
+      }
+      body = JSON.stringify(obj);
+    } catch (error) {
+      console.log(`高德地图-首页卡片, 出现异常`);
+    }
+    break;
+  case "高德地图-首页消息":
+    try {
+      let obj = JSON.parse(body);
+      if (obj.msgs) {
+        obj.msgs = [];
+      } else if (obj.data.pull3 && obj.data.pull3.msgs) {
+        obj.data.pull3.msgs = [];
+      }
+      body = JSON.stringify(obj);
+    } catch (error) {
+      console.log(`高德地图-首页消息, 出现异常`);
+    }
+    break;
+  case "高德地图-我的":
+    try {
+      let obj = JSON.parse(body);
+      if (obj.data && obj.data.cardList) {
+        for (let i = 0; i < obj.data.cardList.length; i++) {
+          obj.data.cardList.localCache = false;
+        }
+        obj.data.cardList = obj.data.cardList.filter((item) => {
+          return (
+            // item.dataKey === "AnnualBillCardV2" || // 年度报告
+            item.dataKey === "MyOrderCard" || // 我的订单
+            // item.dataKey === "GdRecommendCard" || // 高德推荐
+            item.dataKey === "SceneVehicleCard_recommend" || // 我的车辆
+            item.dataKey === "SceneVehicleCard_function" // 我的车辆
+            // item.dataKey === "PopularActivitiesCard" // 热门活动
+            // item.dataKey === "GameExcitation" || // 小德爱消除
+            // item.dataKey === "GoodsShelvesCard" || // 精选服务
+            // item.dataKey === "DiyMap_function" || // DIY 地图
+          );
+        });
+      }
+      body = JSON.stringify(obj);
+    } catch (error) {
+      console.log(`高德地图-我的, 出现异常`);
+    }
+    break;
+  default:
+    break;
+}
+
+$done({ body });
